d168b3f605bce7d19036d0de86741fa258cea79f
Aliaksei Bialiauski <Hialexbel@gmail.com>
2023-04-26 15:37:15 +0300

    whitelist pipe

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const api = await NestFactory.create(ApiModule);
-  api.useGlobalPipes(new ValidationPipe());
+  api.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await api.listen(3000);
 }
 
